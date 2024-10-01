3ad64140b718b1d68ded09f3a5c6e063bbdaee4d
teddywests1de <dirkieys21@gmail.com>
2024-10-01 22:22:53 +0200

    Completed task list

--- src/App.js
+++ src/App.js
@@ -1,17 +1,49 @@
 
+import { useState } from "react";
 import {Footer} from "./Footer";
 import { Header } from "./Header";
 import { Main } from "./Main";
+const users = [
+  {
+    name: "Elrico",
+    age: 43
+  },
+  {
+    name: "Basil",
+    age: 42
+  },
+  {
+    name: "Goofer",
+    age: 40
+  },
+  {
+    name: "Drakie",
+    age: 42
+  },
 
+]
 export function App() {
   const date = new Date();
   return (
     <div className="App">
       <div className="app-container">
         <Header />
-        <Main date={date} />
+        <Main date={date}/>
+        {/* <Users users={users} /> */}
         <Footer date={date} />
       </div>
     </div>
   );
 }
+
+// function Users({users}) {
+//   const [userName, setUserName] = useState('---');
+//   // console.log(userName);
+//   return <div className="users">
+//       <select value={userName} onChange={(e)=>setUserName(e.target.value)}>
+//       {users.map(item => (
+//           <option>{item.name}</option>
+//         ))}
+//       </select>
+//   </div>
+// }
--- src/Main.js
+++ src/Main.js
@@ -1,9 +1,13 @@
 import { useState } from "react";
 
+
 export function Main({date}) {
   const [input, setInput] = useState('');
   const [items, setItems] = useState([])
 
+  function handleAddItems(item) {
+    setItems(items => [...items, item]);
+  }
   function handleSubmit(e) {
     e.preventDefault();
     const id = crypto.randomUUID();
@@ -13,29 +17,53 @@ export function Main({date}) {
       name: input,
       date: date.toDateString(),
       checked: false,
+      assigned: '',
     }
     // console.log(item);
-    setItems(items => [...items, item]);
+    handleAddItems(item);
     setInput('');
-    console.log(items);
+    console.log(item);
   }
 
+  function handleToggleItem(id) {
+    setItems(items => items.map(item => item.id === id ? {...item, checked: !item.checked} : item))
+  }
   return (
     <main className="main">
-      <h4>
+      <h3>
       Today: {date.toDateString()}
-      </h4>
+      </h3>
       <form className="add-form" onSubmit={handleSubmit}>
         <input type="text" placeholder="Enter to do item..." value={input} onChange={(e)=>setInput(e.target.value)}/>
         <button type="submit"><i className="fa-sharp fa-solid fa-plus"></i></button>
       </form>
-      <div className="list-items">
-        <ul>
-          {items.map(item => {
-            <li key={item.id}><span><input type="checkbox" value={item.checked}></input>{item.name}</span><span>{item.date}</span></li>
-          })}
+
+      <div className="item-list">
+        <ul className="list-items">
+        {items.map(item => (
+          <Item item={item} onToggle={handleToggleItem}  key={item.id}/>
+        ))}
         </ul>
       </div>
     </main>
   );
+}
+
+
+function Item({item, onToggle}) {
+
+  return (
+    <li className="list-item">
+      {/* <input type="checkbox" value={item.checked} onChange={()=>onToggle(item.id)}/> */}
+      <i className={item.checked ? "fa-solid fa-square-full" : "fa-regular fa-square-full"} onClick={()=>onToggle(item.id)}></i>
+      <div class="item-body">
+        <span className={item.checked ? "completed" : ""}>{item.name}</span>
+        {/* <span className="small-text">{item.date.slice(4)}</span> */}
+      </div>
+      <div className="item-actions">
+      <i class="fa-solid fa-pencil"></i>
+      <i class="fa-solid fa-xmark"></i>
+      </div>
+    </li>
+  )
 }
